1611601f0b4e31a098fd475d4334d405e27e5139
Khushi-031 <Khushiaggarwal1011@gmail.com>
2026-02-23 14:19:31 +0530

    Add files via upload

---
+++ SETS/Set_in_python.py
@@ -0,0 +1,4 @@
+set1={34,35,89,45,30,99,34}
+print("set is : ")
+print(set1)  #it only print unique values
+
---
+++ SETS/set_operations.py
@@ -0,0 +1,6 @@
+set1={45,43,67,89,70}
+print("first set is :",set1)
+set2={89,34,56,43,22}
+print("set2 is : ",set2)
+set3 =set1.union(set2)
+print("set 3 is :",set3)
